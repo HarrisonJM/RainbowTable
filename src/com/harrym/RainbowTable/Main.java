6489178ac018b071780a42f85a1a4879a1e1c36e
Harrison James HJM. Marcks <harrison.marcks@live.uwe.ac.uk>
2019-12-10 10:13:48 +0000

    Need to rework the algorithm because I misunderstood rainbow tables...

--- src/com/harrym/RainbowTable/Main.java
+++ src/com/harrym/RainbowTable/Main.java
@@ -15,7 +15,7 @@ public class Main {
             RainbowTable rt = new RainbowTable();
             rt.CreateRainbowTable();
         } catch (Exception E) {
-            System.out.println("Threw");
+            System.out.println(E.getMessage());
         }
 
     }
@@ -25,99 +25,93 @@ class RainbowTable {
     private BufferedReader br;
 
     private Set<BigInteger> _hashCollisions;
-    private final int _rows = 50000;
+    private final int _rows = 5000;
     private final int _hashes = 20000;
     private List<List<BigInteger>> _RainbowTable;
+    private BigInteger _mps;
 
     public RainbowTable() throws Exception {
-        File _file = new File(
-                "/home/hmarcks/IdeaProjects/RainbowTable/src/com/harrym/RainbowTable/10-million-password-list-top" +
-                        "-1000000.txt");
+//        File _file = new File(
+//                "/home/hmarcks/IdeaProjects/RainbowTable/src/com/harrym/RainbowTable/10-million-password-list-top" +
+//                        "-1000000.txt");
+        File _file = new File("C:\\Users\\harrym\\IdeaProjects\\RainbowTable\\src\\com\\harrym\\RainbowTable\\10-million-password-list-top-1000000.txt");
+
         FileReader _fr = new FileReader(_file);
         br = new BufferedReader(_fr);
 
         _RainbowTable = new ArrayList<>();
         _hashCollisions = new HashSet<>();
+        int spacePowerLength = 36 ^ 10;
+        _mps = new BigInteger(Integer.toString(spacePowerLength));
     }
 
-    public void CreateRainbowTable() {
+    public void CreateRainbowTable() throws Exception {
         for (int i = 0; i < _rows; i++) {
-            try {
-                String line = br.readLine();
-                var row = GenerateRow(line);
-                System.out.println("Row Number " + i);
-                _RainbowTable.add(row);
-            } catch (Exception e) {
-                // why are you like this
-            }
+            String line = br.readLine();
+            var row = GenerateRow(line);
+            System.out.println("Row Number " + i);
+            _RainbowTable.add(row);
         }
         return;
     }
 
-    public List<BigInteger> GenerateRow(String password) {
+    public List<BigInteger> GenerateRow(String password) throws Exception {
 
         List<BigInteger> row = new ArrayList<>();
         row.add(Hash(password));
         for (int i = 1; i < _hashes; ++i) {
             var reduced = Reduce(row.get(i - 1),
-                                 i);
+                    i);
             if (reduced == null) {
                 row = null;
                 continue;
             }
-            row.add(reduced);
+            row.add(new BigInteger(reduced));
         }
         return row;
     }
 
-    public BigInteger Hash(String stringToHash) {
+    public BigInteger Hash(String stringToHash) throws Exception {
         BigInteger bi = null;
 
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(stringToHash.getBytes(),
-                      0,
-                      stringToHash.length());
-            bi = new BigInteger(1,
-                                md.digest());
-        } catch (Exception e) {
-            // Do nothing lol
-        }
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(stringToHash.getBytes(),
+                0,
+                stringToHash.length());
+        bi = new BigInteger(1,
+                md.digest());
 
         return (BigInteger) CheckCollision(bi);
     }
 
-    public BigInteger Reduce(BigInteger hash) {
+    public BigInteger Reduce(BigInteger hash) throws Exception {
 
         var hashString = hash.toString().substring(0,
-                                                   8);
-
+                8);
         return Hash(hashString);
     }
 
-    public BigInteger Reduce(BigInteger hash, int position) {
-        BigInteger npp = hash;
-        BigInteger MPS = npp.nextProbablePrime();
-        BigInteger posBi = new BigInteger(Integer.toString(Integer.parseInt(hash.toString().substring(0, 4))*position));
-        BigInteger red = posBi.mod(MPS);
-
-        while(red.compareTo(new BigInteger("0")) == 1)
-        {
-            BigInteger index = red.mod(new BigInteger("10"));
+    public String Reduce(BigInteger hash, int position) {
+        BigInteger posBi = new BigInteger(Integer.toString(Integer.parseInt(hash.toString().substring(0, 4)) * position));
+        BigInteger red = posBi.mod(_mps);
+        String password = "";
+        while (red.compareTo(BigInteger.ZERO) == 1) {
+            BigInteger index = red.mod(new BigInteger("36"));
             int index_num = Integer.parseInt(index.toString());
-            String c = hash.toString().substring(index_num);
-            var lastStep = red.add(new BigInteger(c));
-            hash = lastStep.divide(new BigInteger("10"));
+            String foo = hash.toString().substring(index_num, index_num+1);
+            char c = foo.charAt(0);
+            password += c;
+            red = red.divide(new BigInteger("36"));
         }
 
-        return red;
+        return password;
     }
 
     public Object CheckCollision(BigInteger hash) {
         if (_hashCollisions.add(hash)) {
             return hash;
         } else {
-            return hash;
+            return null;
         }
     }
 }
